cd4de2ba18d7678fa679e659eb3c2af85c28fd53
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-19 16:28:30 +0300

    Create and coordinate the container

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+import './index.css';
+
+
+export const Container = ({ children, noSpaces }) => {
+  return (
+    <div
+      className={`main__container ${
+        noSpaces ? '' : 'main__container--padding'
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
