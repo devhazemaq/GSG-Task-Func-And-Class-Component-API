314169f30e225f474ea6aee897514e91c8585967
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-19 16:26:10 +0300

    Create master pages for the router

---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello devhazem </h2>
+        <h3>this is About Page </h3>
+      </div>
+    )
+  }
+}
