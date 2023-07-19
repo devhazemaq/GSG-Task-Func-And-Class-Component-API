ce4e0f7cbdb451306134dc383f39dd14ba1aae5f
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-19 16:41:20 +0300

    A file for static objects and for me to use them more smoothly

---
+++ src/constants/posts.js
@@ -0,0 +1,32 @@
+
+export const POSTS_COLUMNS =  [
+  {
+    key: 'id',
+    title: 'Id',
+  },
+  {
+    key: 'name',
+    title: 'Name',
+  },
+  {
+    key: 'cities',
+    title: 'Cities',
+  },
+  
+];
+
+
+export const INPUTS_ARRAY = [
+  {
+    id: 'name',
+    name: 'name',
+    type: 'text',
+    label: 'Name',
+  },
+  {
+    id: 'cities',
+    name: 'cities',
+    type: 'text',
+    label: 'Cities',
+  },
+]
