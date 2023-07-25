d222717838f1f1e6671edf69d240e6eca1076536
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-25 18:50:40 +0300

    plah

--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -5,40 +5,40 @@ import axios from 'axios';
 
 
 function Store(props) {
-  const [store, setStore] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [store, setStore] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const fetchData = async () => {
-      try {
-          const response = await axios.get(`https://some-data.onrender.com/stores/${props.params.id}`);
-          const data = response.data;
-          setStore(data);
-      } catch (error) {
-          setError('Error While Fetching')
-      } finally {
-          setIsLoading(false);
-      }
-  };
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`https://some-data.onrender.com/stores/${props.params.id}`);
+            const data = response.data;
+            setStore(data);
+        } catch (error) {
+            setError('Error While Fetching')
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  useEffect(() => {
-      fetchData();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.params.id]);
+    useEffect(() => {
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.params.id]);
 
-  return (
-      <div>
-          {!isLoading && (
-              <div className="store__details">
-                  <h1>Details:</h1>
-                  <p>Store Id: {store.id}</p>
-                  <p>Store Name: {store.name}</p>
-                  <p>Store Location: {store.cities}</p>
-                  <span className="error__message">{error}</span>
-              </div>
-          )}
-      </div>
-  );
+    return (
+        <div>
+            {!isLoading && (
+                <div className="store__details">
+                    <h1>Details:</h1>
+                    <p>Store Id: {store.id}</p>
+                    <p>Store Name: {store.name}</p>
+                    <p>Store Location: {store.cities}</p>
+                    <span className="error__message">{error}</span>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default WithParams(Store);
